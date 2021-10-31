f1e30b099aab6547ea7e2da452cc48d70963cb60
Ahsan-Ehtesham <ahsanehtesham110@gmail.com>
2021-11-01 04:21:39 +0500

    Charts updated

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 const App = () => {
   const [holders, setHolders] = useState([]);
   const [rates, setRates] = useState([]);
+  const [chartData, setChartData] = useState({});
 
   //Add btn function
   // const onAdd = (product) => {
@@ -40,26 +41,76 @@ const App = () => {
   // };
 
   const getTokenHolders = async () => {
-    const holderUrl = "https://api.covalenthq.com/v1/1/tokens/0xD417144312DbF50465b1C641d016962017Ef6240/token_holders/?key=ckey_3ef3cefb5f2447cabfdc7d26599&page-size=10";
+    const holderUrl =
+      "https://api.covalenthq.com/v1/1/tokens/0xD417144312DbF50465b1C641d016962017Ef6240/token_holders/?key=ckey_3ef3cefb5f2447cabfdc7d26599&page-size=10";
 
     const response = await fetch(holderUrl);
     const parsedData = await response.json();
-    console.log(parsedData.data.items);
+    // console.log(parsedData.data.items);
     setHolders(parsedData.data.items);
   };
 
   const getRateRequest = async () => {
-    const rateUrl = "https://api.covalenthq.com/v1/pricing/tickers/?tickers=usd%2Ceth%2Cbnb%2Cbtc&key=ckey_3ef3cefb5f2447cabfdc7d26599";
+    const rateUrl =
+      "https://api.covalenthq.com/v1/pricing/tickers/?tickers=usd%2Ceth%2Cbnb%2Cbtc&key=ckey_3ef3cefb5f2447cabfdc7d26599";
 
     const response = await fetch(rateUrl);
     const parsedData = await response.json();
-    console.log(parsedData.data.items);
+    // console.log(parsedData.data.items);
     setRates(parsedData.data.items);
+  };
+
+  const fetchPrices = async () => {
+    const response = await fetch(
+      "https://api.covalenthq.com/v1/1/address/0x0f51bb10119727a7e5ea3538074fb341f56b09ad/portfolio_v2/?key=ckey_3ef3cefb5f2447cabfdc7d26599"
+    );
+    const parsedData = await response.json();
+    // console.log(parsedData.items);
+    setChartData({
+      labels: parsedData.items[0].holdings.map((crypto) =>
+        new Date(crypto.timestamp).toLocaleDateString()
+      ),
+      datasets: [
+        {
+          label: "Price in USD",
+          data: parsedData.items[0].holdings.map((crypto) => crypto.quote_rate),
+          borderColor: "#00506c",
+          color:'#fff',
+          pointBackgroundColor: "rgb(242, 185, 44)",
+          pointRadius: 5,
+          pointStyle:'circle',
+          fill: false,
+          scaleFontColor:"#fff",
+          scales: {
+            xAxes: [
+              {
+                gridLines: {
+                  display: true,
+                  color:'#fff',
+                  scaleFontColor:"#fff",
+                },
+              },
+            ],
+            yAxes: [
+              {
+                gridLines: {
+                  display: false,
+                  color:'#fff',
+                  scaleFontColor:"#fff",
+                },
+              },
+            ],
+          },
+          // backgroundColor: ["rgb(242, 185, 44,0.75)"],
+        },
+      ],
+    });
   };
 
   useEffect(() => {
     getTokenHolders();
     getRateRequest();
+    fetchPrices();
   }, []);
 
   return (
@@ -67,9 +118,13 @@ const App = () => {
       <Router>
         <Navbar />
         <Switch>
-          <Route path="/">
+          <Route exact path="/">
             <Sidebar />
-            <BodySection holders={holders} rates={rates}/>
+            <BodySection
+              holders={holders}
+              rates={rates}
+              chartData={chartData}
+            />
           </Route>
         </Switch>
       </Router>
--- src/components/BodySection.js
+++ src/components/BodySection.js
@@ -1,34 +1,42 @@
-import React, { useEffect } from "react";
+import React from "react";
+import { Line } from "react-chartjs-2";
 // import { Link} from "react-router-dom";
 // import PropTypes from "prop-types";
 // import { useHistory } from "react-router";
 const BodySection = (props) => {
   // const history = useHistory();
   // history.go(0)
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "/js/script.js";
-    script.async = true;
-    document.body.appendChild(script);
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
+
+  // useEffect(() => {
+  //   const script = document.createElement("script");
+  //   script.src = "/js/script.js";
+  //   script.async = true;
+  //   document.body.appendChild(script);
+  //   return () => {
+  //     document.body.removeChild(script);
+  //   };
+  // }, []);
+
   return (
     <div>
-      <section className="col-md-9 ms-sm-auto col-lg-10 px-md-3">
+      <section className="col-md-9 ms-sm-auto col-lg-10 px-md-3 mb-5">
         <div className="container">
           <div className="row">
             {props.rates.map((rate) => {
               return (
-                <div className="col-lg-3 col-sm-6">
+                <div className="col-lg-3 col-sm-6" key={rate.id}>
                   <div className="card-box">
                     <div className="inner">
                       <h3>{rate.quote_rate}</h3>
-                      <p>{rate.contract_name} ({rate.contract_ticker_symbol})</p>
+                      <p>
+                        {rate.contract_name} ({rate.contract_ticker_symbol})
+                      </p>
                     </div>
                     <div className="icon">
-                      <i className="fas fa-hand-holding-usd" aria-hidden="true"></i>
+                      <i
+                        className="fas fa-hand-holding-usd"
+                        aria-hidden="true"
+                      ></i>
                     </div>
                     <a href="/" className="card-box-footer">
                       View More <i className="fa fa-arrow-circle-right"></i>
@@ -37,56 +45,19 @@ const BodySection = (props) => {
                 </div>
               );
             })}
-
-            {/* <div className="col-lg-3 col-sm-6">
-              <div className="card-box bg-green">
-                <div className="inner">
-                  <h3> ₹185358 </h3>
-                  <p> Today’s Collection </p>
-                </div>
-                <div className="icon">
-                  <i className="fa fa-money" aria-hidden="true"></i>
-                </div>
-                <a href="/" className="card-box-footer">
-                  View More <i className="fa fa-arrow-circle-right"></i>
-                </a>
-              </div>
-            </div> */}
-            {/* <div className="col-lg-3 col-sm-6">
-              <div className="card-box bg-orange">
-                <div className="inner">
-                  <h3> 5464 </h3>
-                  <p> New Admissions </p>
-                </div>
-                <div className="icon">
-                  <i className="fa fa-user-plus" aria-hidden="true"></i>
-                </div>
-                <a href="/" className="card-box-footer">
-                  View More <i className="fa fa-arrow-circle-right"></i>
-                </a>
-              </div>
-            </div> */}
-            {/* <div className="col-lg-3 col-sm-6 boxes">
-              <div className="card-box bg-red">
-                <div className="inner">
-                  <h3> 723 </h3>
-                  <p> Faculty Strength </p>
-                </div>
-                <div className="icon">
-                  <i className="fa fa-users"></i>
-                </div>
-                <a href="/" className="card-box-footer">
-                  View More <i className="fa fa-arrow-circle-right"></i>
-                </a>
-              </div>
-            </div> */}
           </div>
         </div>
       </section>
 
-      <section className="col-md-9 ms-sm-auto col-lg-10 px-md-4" id="tokenTable">
-        <div className="my-4 w-100" id="myChart"></div>
-        <h2>Top 10 Covalent Token Holders</h2>
+      <section
+        className="col-md-9 ms-sm-auto col-lg-10 px-md-4"
+        id="tokenTable"
+      >
+        {/* <div className="my-4 w-100" id="myChart"></div> */}
+        <div>
+          <Line data={props.chartData} />
+        </div>
+        <h2 className="mt-5">Top 10 Covalent Token Holders</h2>
         <div className="table-responsive">
           <table className="table table-striped table-sm table-hover">
             <thead>
@@ -99,7 +70,7 @@ const BodySection = (props) => {
             </thead>
             {props.holders.map((holder) => {
               return (
-                <tbody>
+                <tbody key={holder.id}>
                   <tr>
                     <td>{holder.address}</td>
                     <td>{holder.contract_ticker_symbol}</td>
